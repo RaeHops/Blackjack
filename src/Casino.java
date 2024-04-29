b19fb16b88c6649493b21a6faa975891120aef13
RHopkins26 <raegan.hopkins26@milton.edu>
2024-04-29 13:10:26 -0400

    Blackjack game start

---
+++ src/Casino.java
@@ -0,0 +1,11 @@
+public class Casino {
+    public static void main(String[] args) {
+        Casino c = new Casino();
+
+    }
+
+    public Casino(){
+        System.out.println("Welcome to Gigi's Casino!");
+    }
+
+}
---
+++ src/Player.java
@@ -0,0 +1,9 @@
+public class Player {
+
+    //current value of cards
+    //what cards yoy have - hand
+    //player name
+    //is dealer or player
+    //Chose to hit
+    //chose to stay
+}
