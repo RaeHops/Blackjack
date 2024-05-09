58e707b70eeef9f48fca3f9b9bcf02fe58de7074
RHopkins26 <raegan.hopkins26@milton.edu>
2024-05-09 09:06:11 -0400

    Caught up

--- src/Casino.java
+++ src/Casino.java
@@ -1,4 +1,7 @@
 public class Casino {
+
+    public Card [] deck;
+
     public static void main(String[] args) {
         Casino c = new Casino();
 
@@ -6,6 +9,50 @@ public class Casino {
 
     public Casino(){
         System.out.println("Welcome to Gigi's Casino!");
+        makeDeck();
+
+       //shuffle first
+        shuffle();
+        // then distribute cards
+        printDeck();
     }
 
-}
+    public void makeDeck(){
+        deck = new Card[52];
+        int count = 0;
+
+
+        //fill our deck with cards
+        for(int t = 0; t < 13; t++){
+            for(int s = 0; s < 4; s++){
+                deck[count] = new Card(t, s);
+                deck[count].print();
+                count++;
+
+            }
+        }
+    }
+
+    public void shuffle(){
+        //for loop
+        for(int x = 0; x < 52; x++){
+            //grab random card and swap with current index (x)
+            int randomIndex = (int)(Math.random()* 52); //generate 0-51
+            Card randomCard = deck[randomIndex];
+
+            //swap
+            Card temp = deck[x];
+            deck[x] = randomCard;
+            deck[randomIndex] = temp;
+        }
+    }
+
+    public void printDeck(){
+        for (int i = 0; i<deck.length; i++){
+            deck[i].print();
+        }
+    }
+
+    }
+
+
